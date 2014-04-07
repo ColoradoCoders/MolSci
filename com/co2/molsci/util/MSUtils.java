bad70f7dde22a2af79c221c058aabab718f059b4
defiant810 <defiant810@gmail.com>
2014-04-07 16:40:14 -0600

    Putting the MSUtils back in the right spot.

---
+++ com/co2/molsci/util/MSUtils.java
@@ -0,0 +1,29 @@
+package com.co2.molsci.util;
+
+import net.minecraft.item.ItemStack;
+
+public class MSUtils 
+{
+	public static boolean arrayContains(Object[] arr, Object obj)
+	{
+		for (Object o : arr)
+			if (o.equals(obj))
+				return true;
+		
+		return false;
+	}
+	
+	public static boolean itemStackMatch(ItemStack s1, ItemStack s2)
+	{
+		return (s1.getItem() == s2.getItem()) && (s1.getItemDamage() == s2.getItemDamage());
+	}
+	
+	public static boolean itemStackArrayContains(ItemStack[] arr, ItemStack stack)
+	{
+		for (ItemStack is : arr)
+			if (itemStackMatch(is, stack))
+				return true;
+		
+		return false;
+	}
+}
