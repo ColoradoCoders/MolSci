7240de80069ca333e33876fc95178a12745498d9
imdodrill <imdodrill100@gmail.com>
2014-04-10 17:32:35 -0800

    research manager

--- src/main/java/com/co2/molsci/util/ResearchManager.java
+++ src/main/java/com/co2/molsci/util/ResearchManager.java
@@ -1,5 +1,44 @@
 package com.co2.molsci.util;
 
-public class ResearchManager {
-	//NBTTagList test = new NBTTagList();
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagList;
+
+public class ResearchManager
+{
+	NBTTagList test = new NBTTagList();
+	
+	public void addBuff(EntityPlayer player, String category, byte choice) {
+		player.getEntityData().getByteArray(category)[getLevel(player, category)] = choice;
+	}
+	
+	public void addRP(EntityPlayer player, String category, int amount)
+	{
+		player.getEntityData().setInteger(category, amount);
+	}
+	
+	public byte getBuff(EntityPlayer player, String category)
+	{
+		return player.getEntityData().getByteArray(category)[getLevel(player, category)];
+	}
+	
+	public int getLevel(EntityPlayer player, String category)
+	{
+		int total = player.getEntityData().getInteger(category);
+		if(total < 500)
+			return 0;
+		else if(total < 1500)
+			return 1;
+		else if(total < 3000)
+			return 2;
+		else if(total < 5000)
+			return 3;
+		else if(total < 10000)
+			return 4;
+		else
+			return 5;
+	}
+	
+	public int getSpecificLevel(EntityPlayer player, String category) {
+		return player.getEntityData().getInteger(category);
+	}
 }
